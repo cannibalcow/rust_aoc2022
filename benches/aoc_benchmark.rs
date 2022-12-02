4a384bf0e16978387a265c3875122dd6928f7a73
daheld <daheld@lm.se>
2022-12-02 09:48:44 +0100

    day2 part1

--- benches/aoc_benchmark.rs
+++ benches/aoc_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use rust_aoc2022::days::{Day1, Solution};
+use rust_aoc2022::days::{Day1, Day2, Solution};
 
 fn day_1_part_1(c: &mut Criterion) {
     let day1 = Day1::new();
@@ -15,5 +15,12 @@ fn day_1_part_2(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, day_1_part_1, day_1_part_2);
+fn day_2_part_1(c: &mut Criterion) {
+    let day2 = Day2::new();
+    c.bench_function("Day 2 Part 1", |b| {
+        b.iter(|| day2.solve_part1());
+    });
+}
+
+criterion_group!(benches, day_1_part_1, day_1_part_2, day_2_part_1);
 criterion_main!(benches);
--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,24 +1,141 @@
 use crate::days::aoc::Solution;
 
-use super::aoc::Answer;
+use super::{
+    aoc::{get_path, read_file_str, Answer},
+    Files,
+};
 
 pub struct Day2 {}
+
+#[derive(Debug)]
+enum HandResult {
+    Win,
+    Lose,
+    Draw,
+}
+
+trait Score {
+    fn score(&self) -> i32;
+}
+
+impl Score for HandResult {
+    fn score(&self) -> i32 {
+        match self {
+            HandResult::Win => 6,
+            HandResult::Lose => 0,
+            HandResult::Draw => 3,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Hand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Score for Hand {
+    fn score(&self) -> i32 {
+        match self {
+            Hand::Rock => 1,
+            Hand::Paper => 2,
+            Hand::Scissors => 3,
+        }
+    }
+}
+
+trait Beats {
+    fn beats(&self) -> Self;
+}
+
+impl Hand {
+    pub fn parse_round(round: &str) -> (Hand, Hand) {
+        let mut split_hand = round.split_whitespace();
+        let left_hand = match split_hand.next() {
+            Some("A") => Hand::Rock,
+            Some("B") => Hand::Paper,
+            Some("C") => Hand::Scissors,
+            Some(&_) => panic!("Uknown hand type"),
+            None => panic!("Unparsable data :("),
+        };
+
+        let right_hand = match &split_hand.next() {
+            Some("X") => Hand::Rock,
+            Some("Y") => Hand::Paper,
+            Some("Z") => Hand::Scissors,
+            Some(&_) => panic!("Uknown hand type"),
+            None => todo!(),
+        };
+
+        return (left_hand, right_hand);
+    }
+
+    pub fn play(player_hand: &Hand, elf_hand: &Hand) -> (HandResult, i32) {
+        let (player_beats, elf_beats) = (&player_hand.beats(), &elf_hand.beats());
+
+        let result = match (player_beats, elf_beats) {
+            _ if player_beats == elf_hand => HandResult::Win,
+            _ if elf_beats == player_hand => HandResult::Lose,
+            _ => HandResult::Draw,
+        };
+        /*
+        println!(
+            "Score: {:?}: {:?} {:?}: {:?} = {:?}",
+            &result,
+            &result.score(),
+            &player_hand,
+            &player_hand.score(),
+            &result.score() + &player_hand.score()
+        );
+        */
+        let score = &result.score() + &player_hand.score();
+        return (result, score);
+    }
+}
+
+impl Beats for Hand {
+    fn beats(&self) -> Self {
+        match *self {
+            Hand::Rock => Hand::Scissors,
+            Hand::Paper => Hand::Rock,
+            Hand::Scissors => Hand::Paper,
+        }
+    }
+}
 
 impl Day2 {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn solve1(data: &String) -> i32 {
+        return data
+            .lines()
+            .map(|round| {
+                let hand = Hand::parse_round(round);
+                let result = Hand::play(&hand.1, &hand.0);
+                return result;
+            })
+            .map(|result| result.1)
+            .into_iter()
+            .sum();
     }
 }
 
 impl Solution for Day2 {
     fn solve_example1(&self) -> Answer {
         let instant = self.timer_start();
-        return Answer::new("123456", instant.elapsed());
+        let data = read_file_str(&get_path(Files::Example1, self.get_day()));
+        let solution = Day2::solve1(&data);
+        return Answer::new(&solution.to_string(), instant.elapsed());
     }
 
     fn solve_part1(&self) -> Answer {
         let instant = self.timer_start();
-        return Answer::new("123456", instant.elapsed());
+        let data = read_file_str(&get_path(Files::Part1, self.get_day()));
+        let solution = Day2::solve1(&data);
+        return Answer::new(&solution.to_string(), instant.elapsed());
     }
 
     fn solve_example2(&self) -> Answer {
